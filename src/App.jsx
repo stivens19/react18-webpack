90aa9a9f9555eb1812765711927ebe2c32a5abf7
Stivens Espinoza Reina <stivens70espinoza@gmail.com>
2022-04-20 07:11:09 -0500

    template react-18 webpack

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React,{useEffect} from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="bienvenida">
+        <img className="girar" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="imagen react" />
+        <h1>Bienvenido a React 18</h1>
+        <span>Empecemos !!!</span>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path=require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+module.exports={
+    entry:'./src/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js',
+        publicPath:'/'
+    },
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                }
+            },
+            {
+                test:/\.html$/,
+                use:{
+                    loader:'html-loader'
+                }
+            },
+            {
+                test:/\.(s*)css$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'assets/[name].css'
+        }),
+        new Dotenv()
+    ],
+    devServer:{
+        static:path.join(__dirname,'dist'),
+        compress:true,
+        port:3005,
+        historyApiFallback:true,
+        open:true
+    }
+}
